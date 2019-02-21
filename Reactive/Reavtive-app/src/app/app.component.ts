3f25de015157ee4601190bebb6ec2417c34d3ced
Tekeshwar Bisen <tekeshwar.bisen@impetus.co.in>
2019-02-21 12:57:32 +0530

    Reactive app

---
+++ Reactive/Reavtive-app/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  registrationForm = new FormGroup({
+    name: new FormControl('Test name'),
+    userEmail: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      postalCode: new FormControl()
+    })
+  });
+  loadApiData(){
+    this.registrationForm.setValue({
+      name:'Ashok',
+      userEmail:'Ashok@impetus.co.in',
+      password:'123',
+      confirmPassword:'123',
+      address:{
+        city:'Indore',
+        postalCode:'410001'
+      }
+    });
+
+  }
+
+}
